ad8e2aa3e2b34c71f42e9de52938a243c73779e3
nxhoaf <nxhoaf@gmail.com>
2012-12-20 01:21:41 +0100

    Some test class

---
+++ chap01_declarations_and_access_control/Foo_55.java
@@ -0,0 +1,12 @@
+class Foo_55 {
+	int size = 27;
+	public void setSize(int size) {
+		size = size;
+		System.out.println("Size: " + size);
+	}
+	
+	public static void main(String[] args) {
+		Foo_55 foo = new Foo_55();
+		foo.setSize(10);
+	}
+}
