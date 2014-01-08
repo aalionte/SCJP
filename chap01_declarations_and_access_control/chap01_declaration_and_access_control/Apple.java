e6d341bad30c644f2bcaa961406dfeba5a98f06c
nxhoaf <nxhoaf@gmail.com>
2014-01-09 01:18:17 +0700

    GrandParent, Parent and child in different classes

--- chap01_declarations_and_access_control/chap01_declaration_and_access_control/Apple.java
+++
@@ -1,7 +0,0 @@
-package chap01_declaration_and_access_control;
-
-import food.Fruit;
-
-class Apple extends Fruit {
-
-}
--- chap01_declarations_and_access_control/chap01_declaration_and_access_control/Modifier.java
+++
@@ -1,5 +0,0 @@
-package chap01_declaration_and_access_control;
-
-public strictfp abstract  class Modifier {
-
-}
